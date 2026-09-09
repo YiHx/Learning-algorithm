5eb6c734ed6283bb44e2e91ae6d3ccf600b84c27
YiHx <yhx2582413@gmail.com>
2026-09-09 08:32:18 +0800

    update

--- CountConsistentKeys.cpp
+++ CountConsistentKeys.cpp
@@ -1,1 +1,21 @@
 #include <bits/stdc++.h>
+using namespace std;
+vector<vector<int>> b;
+vector<vector<int>> a;
+void solve () {
+    int n,m;
+    cin>>m>>n;
+    for (int i=0;i<m;i++) {
+        int curr;
+        cin>>curr;
+        for (int j=0;j<curr;j++) {
+            
+        }
+    }
+}
+int main () {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    solve();
+    return 0;
+}
