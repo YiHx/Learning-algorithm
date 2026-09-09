8f11738ab02f0e581d728c25b0b623d64a55ea07
YiHx <yhx2582413@gmail.com>
2026-09-09 22:34:50 +0800

    Complete DeleteAgainAndAgain

--- DeleteAgainAndAgain.cpp
+++ DeleteAgainAndAgain.cpp
@@ -1,5 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int N =1e6+10;
+int stack1[N];
+int stack2[N];
+int size1;
+
 
 vector<int> nextArray(const string&t) {
     int m =t.length();
@@ -24,43 +29,56 @@ vector<int> nextArray(const string&t) {
 
 }
 
-string kmp(const string&s,const string&t) {
+void kmp(const string&s,const string&t) {
     string taget=s;
     int len1=s.length();
     int len2=t.length();
     bool check=true;
     int x=0,y=0;
     vector<int> next =nextArray(t);
-    while (check) {
-        x=0;
-        y=0;
-        len1=taget.length();
-        while (x<len1 && y<len2) {
+
+
+
+        while (x<len1) {
             if (t[y]==taget[x]) {
+
+                stack1[size1]=x;
+                stack2[size1]=y;
+                size1++;
                 x++;
                 y++;
+
             }else if (y==0) {
+                stack1[size1]=x;
+                stack2[size1]=-1;
+                size1++;
                 x++;
             }else {
                 y=next[y];
             }
+            if (y==len2) {
+                size1 -= len2;
+                y= size1>0 ? (stack2[size1-1]+1) : 0;
+            }
         }
-        if (y==len2) {
-            string source1 =taget.substr(0,x-y);
-            string source2 =taget.substr(x);
-            taget=source1+source2;
-        }else {
-            check=false;
-        }
-    }
-    return taget;
+        // if (y==len2) {
+        //     string source1 =taget.substr(0,x-y);
+        //     string source2 =taget.substr(x);
+        //     taget=source1+source2;
+        // }else {
+        //     check=false;
+        // }
+
 }
 void solve() {
     string s,t;
     cin>>s>>t;
 
-    string taget=kmp(s,t);
-    cout<<taget<<endl;
+    kmp(s,t);
+    for (int i=0;i<size1;i++) {
+        cout<<s[stack1[i]];
+    }
+
 }
 int main () {
     ios::sync_with_stdio(0);
