d5bdf8eb14df9b78b8172957e2c41221796927ff
YiHx <yhx2582413@gmail.com>
2026-09-21 12:27:56 +0800

    update

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+void solve(){
+
+}
+using namespace std;
+int main () {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T =1;
+    cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
