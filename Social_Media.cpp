471b6c7112dd2ef88c6884c2da8aecd48598c916
YiHx <yhx2582413@gmail.com>
2026-09-24 22:16:58 +0800

    update

--- Social_Media.cpp
+++ Social_Media.cpp
@@ -2,79 +2,73 @@
 using namespace std;
 #define endl '\n'
 
-struct Node {
-    int a1;
-    int a2;
-    int cnt = 0;
-};
-
-struct Compare {
-    bool operator()(struct Node & a, struct Node & b) {
-        return a.cnt < b.cnt;
-    }
-};
 
 void solve(){
     int n,m,k;
     cin>>n>>m>>k;
+    vector<bool> ok(k+1,false);
     long long ans = 0;
-    vector<bool> a(k+1,false);
-    for (int i=1;i<=n;i++) {
-        int curr;cin>>curr;
-        a[curr]=true;
+    for(int i=1;i<n+1;i++){
+        int curr;
+        cin>>curr;
+        ok[curr] = true;
     }
-
+    vector<int> a(k+1,0);
     map<pair<int,int>,int> mp;
-    // pair<int,int> pr;
-    vector<pair<int,int>> all;
-    priority_queue<Node,vector<Node>,Compare> pq;
-    // vector<Node> b;
-    for (int i=1;i<=m;i++) {
-        vector<int> curr(2);
-        cin>>curr[0]>>curr[1];
-        all.push_back({curr[0],curr[1]});
-        if (a[curr[0]]&&a[curr[1]]) {
 
-        }else {
-            ranges::sort(curr);
-            mp[{curr[0],curr[1]}] ++;
+    for(int i = 1; i <= m; i++){
+        int first, second;
+        cin >> first >> second;
+        if(first > second){
+            swap(first, second);
         }
-    }
-
-    for (auto it : mp) {
-        pq.push({it.first.first,it.first.second,it.second});
-    }
-
-    if (!pq.empty()) {
-        Node thisNode = pq.top();
-        pq.pop();
-        if (!a[thisNode.a1]&&!a[thisNode.a2]&&(thisNode.a1==thisNode.a2)||(!a[thisNode.a1]&&a[thisNode.a2]||a[thisNode.a1]&&!a[thisNode.a2])) {
-            a[thisNode.a1]=true;
-            a[thisNode.a2]=true;
-            // ans+=thisNode.cnt;
-            if (!pq.empty()) {
-
-                thisNode = pq.top();
-                pq.pop();
-                if (!a[thisNode.a1]&&!a[thisNode.a2]&&(thisNode.a1==thisNode.a2)||(!a[thisNode.a1]&&a[thisNode.a2]||a[thisNode.a1]&&!a[thisNode.a2])) {
-                    a[thisNode.a1]=true;
-                    a[thisNode.a2]=true;
-                    // ans+=thisNode.cnt;
-                }
-            }
-        }else {
-            a[thisNode.a1]=true;
-            a[thisNode.a2]=true;
-            // ans+=thisNode.cnt;
-        }
-    }
-    for (auto it : all) {
-        if (a[it.first]&&a[it.second]) {
+        if(ok[first] && ok[second]){
             ans++;
-        }
+        } else if(ok[first] && !ok[second]){
+            a[second]++;
+        } else if(!ok[first] && ok[second]){
+            a[first]++;
+        } else {
+            if(first == second){
+                a[first]++;
+            } else {
+                mp[{first, second}]++;
+            }
+        } 
     }
-    cout<<ans<<endl;
-}
+    
+        vector<int> all;
+        for(int i=1;i<=k;i++){
+            if(!ok[i]){
+                all.push_back(a[i]);
+            }
+        }
+
+        sort(all.begin(), all.end(), greater<int>());
+
+
+        int maxx = 0;
+        if(all.size()>=1){
+            maxx = max(maxx,all[0]);
+        }
+
+        if(all.size()>=2){
+            maxx = max(maxx,all[0]+all[1]);
+        }
+
+        for(auto const& [edge ,weight] : mp){
+            int oneone = edge.first;
+            int onetwo = edge.second;
+            int total = a[oneone] + a[onetwo] +weight;
+            maxx = max(maxx,total);
+
+        }
+
+        cout<<ans + maxx<<endl;
+
+
+
+    }
 int main () {
     ios::sync_with_stdio(0);
     cin.tie(0);
